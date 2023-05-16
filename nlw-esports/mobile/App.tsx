2c37a8f20f160de8eb2298ea1339e270c29012ab
Matheus Jurkovich <77711064+matheusjurkovich@users.noreply.github.com>
2023-05-16 15:18:55 -0300

    Initial Commit

---
+++ nlw-esports/mobile/App.tsx
@@ -0,0 +1,31 @@
+import { StatusBar } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black
+} from "@expo-google-fonts/inter"
+import { Routes } from './src/routes';
+import { Loading } from './components/Loading';
+import { Background } from './components/Background';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black
+  });
+
+  return (
+    <Background >
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Routes /> : <Loading /> }
+    </Background>
+  );
+}
---
+++ nlw-return/README.md
@@ -0,0 +1,14 @@
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML
+- CSS
+- JavaScript
+- [ScrollReveal](https://scrollrevealjs.org/)
+
+## 💻 Projeto
+
+DoctorCare é uma página institucional no formato One Page, responsiva, para usar em diversos tipos de micros, pequenas e médias empresas. Contém as seguintes seções: Header, Navigation, Home, Sobre, Serviços e Footer
+
+Feito com 💜 by Rocketseat
